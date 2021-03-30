cab600a933d08a6af807d1113a77d1c0aa5739d6
Neil Henning <neil.henning@hey.com>
2021-03-30 20:19:04 +0100

    Try build LLVM on mac.

--- src/llvm/extras.cpp
+++
@@ -1,1 +0,0 @@
-int some_stupid_symbol() { return 42; }
--- src/llvm/mod.rs
+++ src/llvm/mod.rs
@@ -1,12 +1,11 @@
 use crate::*;
 use std::io::{Seek, Write};
 
-#[allow(clippy::upper_case_acronyms)]
-pub struct LLVM {}
+pub struct Llvm {}
 
 pub enum Error {}
 
-impl CodeGen for LLVM {
+impl CodeGen for Llvm {
     type Error = Error;
 
     fn generate<W: Seek + Write>(_library: &Library, _writer: &mut W) -> Result<(), Self::Error> {
