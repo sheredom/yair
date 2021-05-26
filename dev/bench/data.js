ef1f364bfca6710315cda2ccd8cc31a181fdcb7b
github-action-benchmark <github@users.noreply.github.com>
2021-05-26 15:33:15 +0000

    add Rust Benchmark (cargo) benchmark result for 77e0e7b01f4bb68d65460234821bae1ef50762c2

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1622043195288,
+  "repoUrl": "https://github.com/sheredom/yair",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "sheredom",
+            "username": "sheredom"
+          },
+          "committer": {
+            "name": "sheredom",
+            "username": "sheredom"
+          },
+          "id": "77e0e7b01f4bb68d65460234821bae1ef50762c2",
+          "message": "try run a benchmark on CI",
+          "timestamp": "2021-05-26T14:24:35Z",
+          "url": "https://github.com/sheredom/yair/pull/13/commits/77e0e7b01f4bb68d65460234821bae1ef50762c2"
+        },
+        "date": 1622043194268,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "llvm__benchmarks__tests__bench_splat_adds",
+            "value": 3463975,
+            "range": "± 710923",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
