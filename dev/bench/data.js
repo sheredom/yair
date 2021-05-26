ee0bd4df6ae2cdd48f52eb4a3f821bfcea3bb774
github-action-benchmark <github@users.noreply.github.com>
2021-05-26 15:46:22 +0000

    add Rust Benchmark (cargo) benchmark result for e3c87ba193ffb4c17d5bcee15a67826ab2eedb76

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1622043195288,
+  "lastUpdate": 1622043982365,
   "repoUrl": "https://github.com/sheredom/yair",
   "entries": {
     "Rust Benchmark": [
@@ -25,6 +25,36 @@ window.BENCHMARK_DATA = {
             "name": "llvm__benchmarks__tests__bench_splat_adds",
             "value": 3463975,
             "range": "± 710923",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "611171+sheredom@users.noreply.github.com",
+            "name": "Neil Henning",
+            "username": "sheredom"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e3c87ba193ffb4c17d5bcee15a67826ab2eedb76",
+          "message": "Merge pull request #13 from sheredom/benchmark\n\ntry run a benchmark on CI",
+          "timestamp": "2021-05-26T16:41:07+01:00",
+          "tree_id": "8f7812d1710e762389cde110824b9961a2c98e20",
+          "url": "https://github.com/sheredom/yair/commit/e3c87ba193ffb4c17d5bcee15a67826ab2eedb76"
+        },
+        "date": 1622043981415,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "llvm__benchmarks__tests__bench_splat_adds",
+            "value": 2866901,
+            "range": "± 651156",
             "unit": "ns/iter"
           }
         ]
