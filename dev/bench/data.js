f01bc4ff5323129ca4199250e0c1c5a18ece7a67
github-action-benchmark <github@users.noreply.github.com>
2021-10-27 20:00:57 +0000

    add Rust Benchmark on ubuntu-18.04 (cargo) benchmark result for c79734b1885fdea5bcdabbdedf4eff1ca8d74633

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635364755430,
+  "lastUpdate": 1635364857652,
   "repoUrl": "https://github.com/sheredom/yair",
   "entries": {
     "Rust Benchmark": [
@@ -9577,6 +9577,72 @@ window.BENCHMARK_DATA = {
             "name": "llvm__benchmarks__tests__bench_splat_adds",
             "value": 2943165,
             "range": "± 366543",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "611171+sheredom@users.noreply.github.com",
+            "name": "Neil Henning",
+            "username": "sheredom"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c79734b1885fdea5bcdabbdedf4eff1ca8d74633",
+          "message": "Merge pull request #36 from sheredom/yalang/make-pointers-strongly-typed-for-debuggability\n\nMake pointers strongly typed (again) for debuggability.",
+          "timestamp": "2021-10-27T20:55:34+01:00",
+          "tree_id": "4638b5f59b5f12794e7ebcc7675aa4aca5198c95",
+          "url": "https://github.com/sheredom/yair/commit/c79734b1885fdea5bcdabbdedf4eff1ca8d74633"
+        },
+        "date": 1635364856330,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "io__benchmarks__tests__create_block",
+            "value": 48,
+            "range": "± 153",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_function",
+            "value": 170,
+            "range": "± 32",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_global",
+            "value": 167,
+            "range": "± 17",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_instruction",
+            "value": 51,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_library",
+            "value": 297,
+            "range": "± 46",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_module",
+            "value": 44,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "llvm__benchmarks__tests__bench_splat_adds",
+            "value": 3359502,
+            "range": "± 643979",
             "unit": "ns/iter"
           }
         ]
