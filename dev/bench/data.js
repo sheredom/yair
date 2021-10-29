c20c89f1b22161484b1818996d096059e7d3ee6a
github-action-benchmark <github@users.noreply.github.com>
2021-10-29 11:56:22 +0000

    add Rust Benchmark on macOS-latest (cargo) benchmark result for d8ec1b922917ff72c9449642af629ab03ce11c7c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635364946260,
+  "lastUpdate": 1635508581715,
   "repoUrl": "https://github.com/sheredom/yair",
   "entries": {
     "Rust Benchmark": [
@@ -4109,6 +4109,68 @@ window.BENCHMARK_DATA = {
             "name": "llvm__benchmarks__tests__bench_splat_adds",
             "value": 3209730,
             "range": "± 278609",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "sheredom",
+            "username": "sheredom"
+          },
+          "committer": {
+            "name": "sheredom",
+            "username": "sheredom"
+          },
+          "id": "d8ec1b922917ff72c9449642af629ab03ce11c7c",
+          "message": "Added some checking around assignments.",
+          "timestamp": "2021-10-27T19:55:38Z",
+          "url": "https://github.com/sheredom/yair/pull/37/commits/d8ec1b922917ff72c9449642af629ab03ce11c7c"
+        },
+        "date": 1635508578552,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "io__benchmarks__tests__create_block",
+            "value": 50,
+            "range": "± 9",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_function",
+            "value": 223,
+            "range": "± 23",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_global",
+            "value": 217,
+            "range": "± 14",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_instruction",
+            "value": 38,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_library",
+            "value": 931,
+            "range": "± 120",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_module",
+            "value": 39,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "llvm__benchmarks__tests__bench_splat_adds",
+            "value": 3310016,
+            "range": "± 234921",
             "unit": "ns/iter"
           }
         ]
