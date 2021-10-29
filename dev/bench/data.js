b69d435391ce482af64dc30b8be5fcaa6c98b803
github-action-benchmark <github@users.noreply.github.com>
2021-10-29 14:51:51 +0000

    add Other benchmarks for windows-latest (customSmallerIsBetter) benchmark result for 32ee2a21db4ae5a457b282515e0bc55cd74eba02

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635519007892,
+  "lastUpdate": 1635519110835,
   "repoUrl": "https://github.com/sheredom/yair",
   "entries": {
     "Rust Benchmark": [
@@ -10931,6 +10931,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "yair-verify",
             "value": 2117696,
+            "unit": "bytes"
+          }
+        ]
+      }
+    ],
+    "Other benchmarks for windows-latest": [
+      {
+        "commit": {
+          "author": {
+            "name": "sheredom",
+            "username": "sheredom"
+          },
+          "committer": {
+            "name": "sheredom",
+            "username": "sheredom"
+          },
+          "id": "32ee2a21db4ae5a457b282515e0bc55cd74eba02",
+          "message": "Try add some code-size benchmarks too.",
+          "timestamp": "2021-10-29T13:30:20Z",
+          "url": "https://github.com/sheredom/yair/pull/38/commits/32ee2a21db4ae5a457b282515e0bc55cd74eba02"
+        },
+        "date": 1635519106075,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bootstrap",
+            "value": 1102336,
+            "unit": "bytes"
+          },
+          {
+            "name": "yair-as",
+            "value": 1284608,
+            "unit": "bytes"
+          },
+          {
+            "name": "yair-dis",
+            "value": 1656320,
+            "unit": "bytes"
+          },
+          {
+            "name": "yair-llvm",
+            "value": 27375616,
+            "unit": "bytes"
+          },
+          {
+            "name": "yair-verify",
+            "value": 1653760,
             "unit": "bytes"
           }
         ]
