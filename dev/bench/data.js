06610d94e2078606c750e1733e303bd8d9889991
github-action-benchmark <github@users.noreply.github.com>
2021-11-01 20:29:40 +0000

    add Rust Benchmark on ubuntu-18.04 (cargo) benchmark result for d2f2574030904325d212dabf1d970538a8264efc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635690495079,
+  "lastUpdate": 1635798580632,
   "repoUrl": "https://github.com/sheredom/yair",
   "entries": {
     "Rust Benchmark": [
@@ -12163,6 +12163,48 @@ window.BENCHMARK_DATA = {
             "name": "llvm__benchmarks__tests__bench_splat_adds",
             "value": 3303952,
             "range": "± 624877",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "neil.henning@hey.com",
+            "name": "Neil Henning",
+            "username": "sheredom"
+          },
+          "committer": {
+            "email": "neil.henning@hey.com",
+            "name": "Neil Henning",
+            "username": "sheredom"
+          },
+          "distinct": true,
+          "id": "d2f2574030904325d212dabf1d970538a8264efc",
+          "message": "Add array parsing.",
+          "timestamp": "2021-11-01T20:26:18Z",
+          "tree_id": "3811055322b226d09b112b7fe3760865427ad8c4",
+          "url": "https://github.com/sheredom/yair/commit/d2f2574030904325d212dabf1d970538a8264efc"
+        },
+        "date": 1635798579242,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "io__benchmarks__tests__create_block",
+            "value": 47,
+            "range": "± 10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_function",
+            "value": 140,
+            "range": "± 24",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "io__benchmarks__tests__create_global",
+            "value": 143,
+            "range": "± 25",
             "unit": "ns/iter"
           }
         ]
